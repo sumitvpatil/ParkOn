d55d7230dfc7d98eb3854c376ce8de4a56c13d26
sumitvpatil <sumit6921@gmail.com>
2023-01-24 23:33:37 +0530

    changed environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrlUser:"https://parkon-server.herokuapp.com/user",
-  baseUrlCompany:"https://parkon-server.herokuapp.com/company"
+  baseUrlUser:"https://parkon-server.onrender.com//user",
+  baseUrlCompany:"https://parkon-server.onrender.com//company"
 };
